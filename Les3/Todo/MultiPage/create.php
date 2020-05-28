6f117a4f31074d45a87245710490cab4c06fb931
Joris Lops <jorislops@gmail.com>
2020-05-28 15:40:12 +0200

    geef errors weer naast input

--- Les3/Todo/MultiPage/create.php
+++ Les3/Todo/MultiPage/create.php
@@ -15,18 +15,18 @@ if(isset($_POST["ACTION"]) && $_POST["ACTION"] === "AddTodo")
     if(!empty($_POST["description"])) {
         $description = filter_var($_POST["description"],FILTER_SANITIZE_STRING);
         if($description === false) {
-            $errors[] = "incorrect description";
+            $errors["description"] = "incorrect description";
             $description = $_POST["description"];
         }
     } else {
-        array_push($errors, "description can not be empty");
+        $errors["description"] = "description can not be empty";
     }
 
     //filter done
     if(!empty($_POST["done"])) {
         $done = filter_var($_POST["done"], FILTER_VALIDATE_BOOLEAN);
         if($done === false) {
-            array_push($errors, "incorrect done");
+            $errors["done"] = "incorrect done";
             $done = $_POST["done"];
         }
     }
@@ -58,20 +58,30 @@ if(isset($_POST["ACTION"]) && $_POST["ACTION"] === "AddTodo")
             }
         }
     } else {
-        echo "<h1>Errors:</h1>";
-        foreach ($errors as $error) {
-            echo $error . "</br>";
-        }
+        echo "<h1>Errors detected</h1>";
     }
 }
+
+function displayError($inputName) {
+   global $errors; //liever geen globals gebruiken! Hier kan het niet anders :-(
+
+   if(isset($errors[$inputName])) {
+       return $errors[$inputName];
+   }
+}
+
 ?>
 
 <h1>Create Todo Item</h1>
 <form method="post">
     <input type="hidden" name="ACTION" value="AddTodo">
 
-    <input name="description" type="text" value="<?= $description ?>">
-    <input name="done" type="checkbox" value="<?= $done ?>" >
+    <?= displayError("description") ?>
+    <input name="description" type="text" value="<?= $description ?>"><br/>
+
+    <?= displayError("done") ?>
+    <input name="done" type="checkbox" value="<?= $done ?>" ><br/>
+
     <button type="submit">Add</button>
 </form>
 </hr>
